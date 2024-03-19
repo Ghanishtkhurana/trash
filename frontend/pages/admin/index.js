e2bae748bc846be344020895b3ecdf484fb9e463
ghanishtkhurana <ghanishtkhurana9@gmail.com>
2024-03-19 23:33:29 +0530

    distance cal done

--- frontend/pages/admin/index.js
+++ frontend/pages/admin/index.js
@@ -1,14 +1,24 @@
 import AdminSidebar from "@/components/admin/AdminSidebar";
 import UserPanel from "@/components/admin/UserPanel";
 import Warehouse from "@/components/admin/Warehouse";
+import DistanceCal from "@/core/DistanceCal";
 import RedirectPrivatePages from "@/redirects/RedirectToPrivate";
 import { jwtDecode } from "jwt-decode";
 import dynamic from "next/dynamic";
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 
 const Admin = () => {
-  //   const pathname = "warehouse";
+  const obj1 = {
+    latitude: 37.7749,
+    longitude: -122.4194,
+  };
+  const obj2 = {
+    latitude: 40.7128,
+    longitude: -74.006,
+  };
+  const [distance, setDistance] = useState(0);
+  console.log("i",distance)
   const { navVal } = useSelector((store) => store.adminSlice);
   console.log("navVal", navVal);
   return (
@@ -18,6 +28,12 @@ const Admin = () => {
           <AdminSidebar />
         </div>
         <div className="flex-1">
+          <DistanceCal
+            setter={setDistance}
+            obj1={obj1}
+            obj2={obj2}
+          />
+
           {navVal === "warehouse" ? <Warehouse /> : <UserPanel />}
         </div>
       </div>
